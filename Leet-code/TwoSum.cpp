9c63a84a168a90bfac5ed23724d693c37ac5c6f6
Vats Alwadhi <104412660+Vatsalwadhi@users.noreply.github.com>
2025-10-24 18:13:23 +0530

    Add files via upload

---
+++ Leet-code/TwoSum.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> hash;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int diff = target - nums[i];
+            if (hash.find(diff) != hash.end())
+            {
+                return {hash[diff], i};
+            }
+            else
+            {
+                hash[nums[i]] = i;
+            }
+        }
+        return {-1, -1};
+    }
+};
